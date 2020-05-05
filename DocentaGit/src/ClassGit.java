6c95d20a9f7de5ba2f260b50ad1f351d6fefb637
camelia84 <57041859+camelia84@users.noreply.github.com>
2020-05-05 09:57:17 +0200

    create new project

---
+++ DocentaGit/src/ClassGit.java
@@ -0,0 +1,10 @@
+
+public class ClassGit {
+	
+    static public int numero;
+    static String nome = "camelia";
+    
+   public static void String (String[] args) {
+	   System.out.println(nome);
+   }
+}
