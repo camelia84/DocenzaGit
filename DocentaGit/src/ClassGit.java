6aa1b2914346f6e86dc038d0048f3609bbe1c411
camelia84 <57041859+camelia84@users.noreply.github.com>
2020-05-05 10:35:52 +0200

    aggiunto nuova variabile

--- DocentaGit/src/ClassGit.java
+++ DocentaGit/src/ClassGit.java
@@ -3,6 +3,7 @@ public class ClassGit {
 	
     static public int numero;
     static String nome = "camelia";
+    static int  dim;
     
    public static void String (String[] args) {
 	   System.out.println(nome);
